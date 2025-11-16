facd6e873418f85390d95747b0b6f0160148a28f
rkrukshan <rukshan1122@gmail.com>
2025-11-16 23:27:18 +0530

    change

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -4,7 +4,6 @@ import { BsBootstrap } from "react-icons/bs";
 import { DiBitbucket, DiDocker, DiDotnet, DiJava, DiJenkins, DiMysql, DiPhp } from "react-icons/di";
 import { GrGithub } from "react-icons/gr";
 import { LuWebhook } from "react-icons/lu";
-import { MdWebhook } from "react-icons/md";
 import { RiNextjsLine, RiReactjsLine, RiTailwindCssFill, RiWebhookFill } from "react-icons/ri";
 import { SiAxios, SiCheckmk, SiFramer, SiInsomnia, SiJenkins, SiMatomo, SiMui, SiN8N, SiNgrok, SiPostman, SiReactrouter, SiRedux, SiSpringboot } from "react-icons/si";
 
@@ -252,7 +251,7 @@ export default function Technologies() {
           variants={iconVariants(2)}
           className="p-4"
         >
-          <MdWebhook className="text-7xl h-29 w-29" />
+          <RiWebhookFill className="text-7xl h-29 w-29" />
         </motion.div>
 
 
