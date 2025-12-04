47f79d91c6e47ab5357383ca922de82d4fc128e2
rkrukshan <rukshan1122@gmail.com>
2025-12-04 16:27:21 +0530

    updated Performance

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,12 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    target: "esnext",
+    cssCodeSplit: true,
+    sourcemap: false,
+    minify: "esbuild",
+  },
 });
